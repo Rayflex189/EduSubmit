09180a8ce5fac5645653234d1d71c7b0004c56fc
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-01-29 20:02:32 +0000

    save

--- assignment_portal/submissions/urls.py
+++ assignment_portal/submissions/urls.py
@@ -1,4 +1,4 @@
-this is the my submission app urls from django.urls import path, include
+from django.urls import path, include
 from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 from . import views
