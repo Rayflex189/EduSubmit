cd1fe64b2ad6b7dd6be339b9cefc58cee9b1e1b0
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-01-29 20:07:26 +0000

    save

--- assignment_portal/assignment_portal/settings.py
+++ assignment_portal/assignment_portal/settings.py
@@ -101,15 +101,8 @@ WSGI_APPLICATION = "assignment_portal.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'sslmode': os.getenv('DB_SSLMODE'),
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
